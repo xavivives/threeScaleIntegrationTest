a909f0cb6b65a2674059e67eca635b1656ff52ac
Xavi <xavings@gmail.com>
2015-06-30 00:01:02 +0200

    Adding 3scale stuff

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var app = express();
 var cool = require ('cool-ascii-faces');
-
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
-
+var Client = require('3scale').Client;
+var providerKey = process.env.THREESCALE_PROVIDER_KEY;
+var appId =  "455a116e";
+var appKey =  "6ae0f31679ab02db77573829ff43e1ed";
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -39,7 +28,23 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  //console.log('Node app is running on port', app.get('port'));
 });
 
+//3scale
 
+client = new Client(providerKey);
+
+var ThreeScale = require('3scale').Client;
+// keep your provider key secret
+var client = new ThreeScale("f42a758238b9927407aeaddd5018b3ff");
+
+client.authrep({ app_id: appId, app_key:appKey }, function(response){
+    console.log("Authotritzation+Report2");
+    console.log(response);
+  if(response.is_success()) {
+    console.log("success");
+  } else {
+    throw new Error("not authorized " + response.error_message);
+  }
+});
